4c25de651144c16fd0b1c10bb05d8d3225822fad
Metranohm <andrewwink@gmail.com>
2022-09-05 13:23:12 -0500

    complete functions

--- js/app.js
+++ js/app.js
@@ -1,27 +1,190 @@
-/*-------------------------------- Constants --------------------------------*/
+/*-------------------- Constants --------------------------------*/
+
+winningCombos = [
+  [0,1,2][3,4,5][6,7,8]
+  [0,3,6][1,4,7][2,5,8]
+  [1,4,7][2,4,6]
+  ]
 
 
-
-/*---------------------------- Variables (state) ----------------------------*/
+/*---------------- Variables (state) ----------------------------*/
 
 let board, turn, winner
-let turnCOunt = 1
-let isWinner = false
-
-/*------------------------ Cached Element References ------------------------*/
-
-const squareEls = document.querySelectorAll(board.class)
-const messageEl = document.getElementById(id.message)
 
 
-/*----------------------------- Event Listeners -----------------------------*/
+/*------------- Cached Element References ------------------------*/
+
+let squareEls = document.querySelectorAll(".board > div")
+let messageEl = document.getElementById('#message')
+let boardEl = document.querySelector('.board')
 
 
+/*------------------- Event Listeners -----------------------------*/
 
-/*-------------------------------- Functions --------------------------------*/
+boardEl.addEventListener('click', handleClick)
+
+/*---------------------- Functions --------------------------------*/
+init()
 
 function init() {
-  board = ['null', 'null','null','null','null','null','null','null','null',]
+  board = [null,null,null,null,null,null,null,null,null,]
+  console.log(board)
   turn = 1
-  
+  winner = null
+  render()
 }
+
+function render() {
+  board.forEach(function(sq, idx) {
+    if (sq === 1) {
+      squareEls[idx].textContent = "P"
+    }else if (sq === -1) {
+      squareEls[idx].textContent = "D"
+    } else {
+      squareEls[idx].textContent = ""
+    }
+  })
+  if (winner === null) {
+    if(turn === 1) {
+      messageEl.textContent = "Pootie Tang, it's your turn!"
+    } else {
+      messageEl.textContent = "Dirty Dee, it's your turn!"
+    }
+  } else if (winner === 'T') {
+    messageEl.textContent = "It's a tingetty tie tangy"
+  } else if (winner === 1) {
+    messageEl.textContent = "Pootie tang wins!"
+  } else if (winner === -1) {
+    messageEl.textContent = "Dirty Dee wins!"
+  }
+}
+
+function handleClick(evt) {
+  const sqIdx = parseInt(evt.target.id[2])
+  if (winner === 1 || winner === -1) {
+    return
+  }
+  if (board[sqIdx]) {
+    return
+  }
+
+  board[sqIdx] = turn
+  turn = turn *-1
+
+  winner = getWinner()
+  render()
+}
+ 
+function getWinner() {
+  let bestCombo = []
+  winningCombos.forEach(function(combo) {
+    let comboValue = board[combo[0]] + board[combo[1]] + board[combo[2]]
+    combo.forEach(function(position) {
+      comboValue += board[position]
+    })
+    bestCombo.push(math.abs(comboValue))
+  })
+  console.log(bestCombo)
+}
+
+
+// Step 4 - The state of the game should be rendered to the user
+
+  // 4a) Create a function called `render`.
+
+  // 4b) Loop over `board` and for each element:
+  //     - Use the current index of the iteration to access the corresponding 
+  //       square in the `squareEls` array.
+  //     - Style that square however you wish, dependent on the value  
+  //       contained in the current cell being iterated over (`-1`, `1`, or
+  //       `null`).  
+  
+  // 4c) Render a message based on the current game state:
+  //     - If winner has a value of `null` (meaning the game is still in
+  //       progress), render whose turn it is.
+  //     - If `winner` is equal to `'T'` (tie), render a tie message.
+  //     - Otherwise, render a congratulatory message to the player that has 
+  //       won.
+    
+
+// Step 5 - Define the required constants
+
+  // 5a) In a constant called `winningCombos` define the eight possible winning 
+  //     combinations as an array of arrays.
+
+// Step 6 - Handle a player clicking a square with a `handleClick` function
+
+  // 6a) Create a function called `handleClick`. It will have an `evt`
+  //     parameter.
+
+  // 6b) Attach an event listener to the game board (you can do this to each
+  //     one of the existing `squareEls` OR add a new cached element reference
+  //     that will allow you to take advantage of event bubbling). On the
+  //     `'click'` event, it should call the `handleClick` function
+  //     you created in 6a.
+
+  // 6c) Obtain the index of the square that was clicked by "extracting" the 
+  //     index from an `id` assigned to the element in the HTML. Assign this  
+  //     to a constant called `sqIdx`.
+
+  // 6d) If the `board` has a value at the `sqIdx`, immediately `return`  
+  //     because that square is already taken. Also, if `winner` is not `null`
+  //     immediately `return` because the game is over.
+
+  // 6e) Update the `board` array at the `sqIdx` with the current value of
+  //     `turn`.
+
+  // 6f) Change the turn by multiplying `turn` by `-1` (this flips a `1` to
+  //     `-1`, and vice-versa).
+
+  // 6g) Set the `winner` variable if there's a winner by calling a new 
+  //     function: `getWinner`.
+
+  // 6h) All the state has been updated so we need to render our updated state 
+  //     to the user by calling the `render` function we wrote earlier.
+
+// Step 7 - Build the `getWinner` function
+
+  // 7a) Create a function called `getWinner`
+
+  /* 
+   * There are two methods you can use to find out if there is a winner.
+   *
+   * Step b1 below is a more elegant method that takes advantage of the
+   * `winningCombos` array you wrote above in step 5. 
+   *
+   * Step b2 might be a little simpler to comprehend, but you'll need to write  
+   * more code. Step b2 also won't take advantage of the `winningCombos`
+   * array, but using it as a reference will help you build a solution.
+   * ***Ensure you choose only one path.***
+   */
+
+  // 7b1)Loop through each of the winning combination arrays defined in the 
+  //     `winningCombos` array. Total up the three board positions using the 
+  //     three indexes in the current combo. Convert the total to an absolute 
+  //     value (convert any negative total to positive). If the total equals 3, 
+  //     we have a winner! Set the `winner` variable to the board's value at
+  //     the index specified by the first index of that winning combination's
+  //     array by returning that value.
+
+  // 7b2)For each one of the winning combinations you wrote in step 5, find the
+  //     total of each winning combination. Convert the total to an absolute 
+  //     value (convert any negative total to positive). If the total equals 3, 
+  //     we have a winner! Set the `winner` variable to the board's value at 
+  //     the index specified by the first index of that winning combination's 
+  //     array by returning that value.
+
+// 7c) If there is no winner, check to see if there is a tie. Set the  
+  //     `winner` variable to `'T'` if there are no more nulls in the board  
+  //     array byreturning the string `'T'`.
+
+  // 7d) If there is no winner and there isn’t a tie, return `null`.
+
+// Step 8 - Create Reset functionality
+
+  // 8a) Add a reset button to the HTML document.
+
+  // 8b) Store the new reset button element in a constant named `resetBtnEl`.
+
+  // 8c) Attach an event listener to the `resetBtnEl`. On the `'click'` event 
+  //     it should call the `init` function you created in 3.
